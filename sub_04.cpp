7b4c1a5df4d09e4de926ca3e30485208d8a03cc6
GicheolL <146831239+GicheolL@users.noreply.github.com>
2024-03-22 14:59:39 +0900

    Add files via upload

--- sub_04.cpp
+++ sub_04.cpp
@@ -2,50 +2,47 @@
 
 int main(void)
 {
-	char student[2][3][20] = 
-	{
-		"Hong Gil Dong",
-		"Computer Electronic",
-		"202100101",
-		"Hong Gil Soon",
-		"Computer Science",
-		"202100201"
-	};
+    char student[2][3][20];
 
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			switch (j)
-			{
-			case 0:
-				printf("학생 %d의 이름 : ", i + 1);
-				break;
-			case 1:
-				printf("학생 %d의 학과 : ", i + 1);
-				break;
-			default:
-				printf("학생 %d의 학번 : ", i + 1);
-				break;
-			}
-			for (int k = 0; k < 20; k++)
-			{
-				printf("%c",student[i][j][k]);
-			}
-			printf("\n");
-			}
-		printf("\n");
-		}
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            switch (j)
+            {
+            case 0:
+                printf("학생 %d의 이름 : ", i + 1);
+                gets_s(student[i][j]);
+                break;
+            case 1:
+                printf("학생 %d의 학과 : ", i + 1);
+                gets_s(student[i][j]);
+                break;
+            default:
+                printf("학생 %d의 학번 : ", i + 1);
+                gets_s(student[i][j]);
+                break;
+            }
+        }
+    }
 
-	for (int i = 0; i < 2; i++)
-	{
-		printf("학생 %d\n", i+1);
-		for(int j = 0; j < 3; j++)
-		{
-			printf("\t%s\n", student[i][j]);
-		}
-		printf("\n");
-	}
+    printf("\n\n");
 
-	return 0;
+    for (int i = 0; i < 2; i++)
+    {
+        printf("학생 %d\n", i+1);
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; student[i][j][k] != '\0'; k++)
+            {
+                printf("%c", student[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    getchar();
+
+    return 0;
 }
